484e4cde2075f5858db667966315c3cfda870eaf
Dylan Heslop <heslopd23@gmail.com>
2023-12-08 14:50:02 -0500

    added metadata and completed project

--- app/dashboard/customers/page.tsx
+++ app/dashboard/customers/page.tsx
@@ -1,5 +1,17 @@
-function Page() {
-  return <h1>Customers Page</h1>;
+import CustomersTable from '@/app/ui/customers/table';
+import { fetchFilteredCustomers } from '@/app/lib/data';
+import { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: 'My Customers',
+};
+async function Page() {
+  const customers = await fetchFilteredCustomers('');
+  return (
+    <>
+      <CustomersTable customers={customers} />
+    </>
+  );
 }
 
 export default Page;
